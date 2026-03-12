3b253cf24c32a8255add434720a5af852dc1cd8a
Dorathy <d17385303@gmail.com>
2026-03-12 02:08:41 +0100

    Create dorathy_info.R

---
+++ dorathy_info.R
@@ -0,0 +1,14 @@
+#!/usr/bin/env Rscript
+
+# Script to print member details for the Bioinformatics assignment
+# Language: R
+
+name <- "Dorathy Christopher"
+email <- "d17385303@gmail.com"
+slack_user <- "@dorathy christopher189"
+interest <- "Transcriptomics"
+
+cat(name, "\n", sep = "")
+cat(email, "\n", sep = "")
+cat(slack_user, "\n", sep = "")
+cat(interest, "\n", sep = "")
